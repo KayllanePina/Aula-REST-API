2752482befce910791597d3c46c61578b82bb1fc
Kayllane <pinakayllane@gmail.com>
2021-11-05 11:55:01 -0300

    adicionar os codigos das aulas

---
+++ README.md
@@ -0,0 +1,31 @@
+# 							Criando uma REST API
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+1. Oque aprendi
+   * Criar uma API nos padrões RESTful
+   * Fazer documentação com o Swagger
+   * Status Code
+   * Recursos, Endpoints e Métodos REST
+   * Versionamento de API
+   * O que DX (Develop Experience)
+   * Prática de Express
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+2. Ferramentas Utilizadas
+   * VS Code
+   * Postman
+   * MySQL Workbench
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+3. Tecnologias utilizadas
+   * Node (Express)
+   * MySQL
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+4. Objetivo da aula
+
+   Criar um sistema de postagem e leitura de notícias, onde o jornalista posta as notícias em um painel administrativo marcando quais são suas categorias (esportes, política, etc...). O leitor por sua vez acessa um site/portal seleciona uma categoria e lê as notícias que deseja.
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+var mysql = require('mysql2');
+const app = express()
+const port = 3000
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'sistema_noticias'
+})
+
+connection.connect()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Serviço de busca de categorias
+app.get('/news-api/v1/categorias', (req, res) => {
+
+  connection.query('SELECT id, nome FROM sistema_noticias.categoria', function (err, rows, fields) {
+    if (err) throw err;
+
+    res.send(rows)
+  })
+})
+
+// Serviço de busca de noticias
+app.get('/news-api/v1/categorias/:categoriaId/noticias', (req, res) => {
+  //Busca noticias de uma categorias 
+  connection.query('SELECT id, titulo FROM sistema_noticias.noticia WHERE id_categoria = ' + req.params.categoriaId, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.send(rows)
+  })
+})
+
+// Sistemas de busca de uma noticias
+app.get('/news-api/v1/categorias/:categoriaId/noticias/:noticiaId', (req, res) => {
+
+  //Busca noticia
+  connection.query('SELECT id, titulo, conteudo FROM sistema_noticias.noticia WHERE id_categoria = ' + req.params.categoriaId + ' AND id = ' + req.params.noticiaId, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.send(rows[0])
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
